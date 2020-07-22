efcbcd4f1460a87197e779ce397df2d087cf16c1
daniel2346 <daniel.agudelo2346@gmail.com>
2020-07-21 20:02:47 -0500

    commit 7

--- catedra_empren.php
+++ catedra_empren.php
@@ -6,6 +6,7 @@
 </head>
 <body>
 <!--hola mundo-->
+<!--HOLA -->
 <div id="mySidebar" class="sidebar">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
   <a href="#">Cátedra de emprendimiento</a>
